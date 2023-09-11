16d8abce97b89535e1f05a7638f642537ea92aa9
Anamika K <anu4anamikaaa@gmail.com>
2023-09-11 09:50:02 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,12 @@
+
+function validation(){
+ let username=document.getElementById('username').value;
+let password=document.getElementById('password').value;
+    if(username=='qwert' && password =='1234')
+    {
+        alert('successful')
+    }
+    else{
+        alert('unsucccesful')
+    }
+}
